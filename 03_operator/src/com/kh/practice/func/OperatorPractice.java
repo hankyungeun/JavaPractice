58c8c5565425796fc286de0027ff76984c67efa1
hangyeong-eun <az2282@naver.com>
2023-12-25 02:15:46 +0900

    12.25과제

--- 03_operator/src/com/kh/practice/func/OperatorPractice.java
+++ 03_operator/src/com/kh/practice/func/OperatorPractice.java
@@ -3,6 +3,44 @@ package com.kh.practice.func;
 import java.util.Scanner;
 
 public class OperatorPractice {
+
+	public void practice3() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 : ");
+		int num = sc.nextInt();
+		if(num / 2 == 0){
+			System.out.println("짝수다");
+		}
+		else
+			System.out.println("홀수다");
+	}
+
+	public void practice4(){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("인원 수 : ");
+		int num = sc.nextInt();
+		System.out.print("사탕 개수 : ");
+		int candy = sc.nextInt();
+
+		System.out.println("1인당 사탕 개수 : " + candy/num);
+		System.out.println("1인당 사탕 개수 : " + candy%num);
+	}
+	
+	public void practice10() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("입력1 : ");
+		int num1 = sc.nextInt();
+		System.out.print("입력2 : ");
+		int num2 = sc.nextInt();
+		System.out.print("입력3 : ");
+		int num3 = sc.nextInt();
+		
+		if(num1 == num2 && num2 == num3) {
+			System.out.println("ture");
+		}
+		else
+			System.out.println("false");
+	}
 	
 	public void practice8() {
 		Scanner sc = new Scanner(System.in);
--- 03_operator/src/com/kh/practice/run/Run.java
+++ 03_operator/src/com/kh/practice/run/Run.java
@@ -1,6 +1,11 @@
 package com.kh.practice.run;
+import com.kh.practice.func.OperatorPractice;
 
 public class Run {
+	public static void main(String[] args) {
+		OperatorPractice operatorPractice = new OperatorPractice();
+		operatorPractice.practice10();
+	}
 
 	
 }
